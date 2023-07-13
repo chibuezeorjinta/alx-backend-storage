0574e1b3611e87f2d55b021a94e32c090c5554cd
daniel orjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-13 09:19:20 +0000

    sql storage

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- create a table with some features that works on all DB's
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
