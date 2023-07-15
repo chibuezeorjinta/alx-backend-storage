1d30cd33ab34534b111a6f84cfd252d2f4794744
daniel orjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-15 14:44:57 +0000

    advanced 1

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- Compute weighted CGPA for students in a database.
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT sum(corrections.score * projects.weight) / sum(projects.weight)
+        FROM corrections
+        INNER JOIN projects
+        ON projects.id = corrections.project_id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END $$
+
+DELIMITER ;
+
