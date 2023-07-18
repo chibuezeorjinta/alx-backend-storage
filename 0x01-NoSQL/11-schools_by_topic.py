daef99ee5f3403aadc69dbea11dad0875425788c
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-18 20:30:03 +0100

    syntax error in task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""use find"""
+"""use find to collect documents containing an item"""
 
 
 def schools_by_topic(mongo_collection, topic):
     """
     use find function on pymongo instance
     """
-    return mongo_collection.find({'topics': {$in: [topic]}})
-
+    return mongo_collection.find({'topics': {'$in': [topic]}})
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,3 +10,8 @@ if __name__ == "__main__":
     print(f'{parse_ngnix.count_documents(filter={})} logs')
     print('Methods:')
     print(f'\tmethod GET: {parse_nginx.count_documents( filter={"method": "GET"} )}')
+    print(f'\tmethod POST: {parse_ngnix.count_documents( filter={"method": "POST"} )}')
+    print(f'\tmethod PUT: {parse_ngnix.count_documents( filter={"method": "PUT"} )}')
+    print(f'\tmethod PATCH: {parse_ngnix.count_documents( filter={"method": "PATCH"} )}')
+    print(f'\tmethod DELETE: {parse_ngnix.count_documents( filter={"method": "DELETE"} )}')
+    print(f'{parse_ngnix.count_documents( filter={"method": "GET", "path": "/status"} )} status check')
