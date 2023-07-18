685ae1afb4c8f593c273f40ff2ba67fdbd98df10
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-07-18 20:34:29 +0100

    mispelling on task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parse_ngnix = client.logs.nginx
     print(f'{parse_ngnix.count_documents(filter={})} logs')
     print('Methods:')
-    print(f'\tmethod GET: {parse_nginx.count_documents( filter={"method": "GET"} )}')
+    print(f'\tmethod GET: {parse_ngnix.count_documents( filter={"method": "GET"} )}')
     print(f'\tmethod POST: {parse_ngnix.count_documents( filter={"method": "POST"} )}')
     print(f'\tmethod PUT: {parse_ngnix.count_documents( filter={"method": "PUT"} )}')
     print(f'\tmethod PATCH: {parse_ngnix.count_documents( filter={"method": "PATCH"} )}')
